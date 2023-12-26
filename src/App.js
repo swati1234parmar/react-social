dff0fcda66f3deab5a207817f861a595baa14059
swati1234parmar <53433458+swati1234parmar@users.noreply.github.com>
2023-12-26 12:50:57 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './pages/home/Home';
+import { Person } from '@mui/icons-material'
+function App() {
+  return (
+    <>
+    <div><Home/></div>
+    <Person />
+    </>
+  );
+}
+
+export default App;
